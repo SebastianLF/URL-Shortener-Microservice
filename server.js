487ddfcadd7e791e0f5c1a244aefbd4401af297e
programGitHub <programdev2000@gmail.com>
2016-06-27 15:44:45 +0200

    start

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.send('URL SHORTENER MICROSERVICE');
+  parseUrl(req.url.substring(1))
 });
 
 app.listen('3000');
+
+const parseUrl = url => {
+	console.log(url)
+	
+	if (checkShort(url))
+		redirectTo(url)
+	else if (checkUrl(url))
+		shortUrl(url)
+	else
+		sendErr('invalid request')
+}
+
+const shortUrl = url => {
+	// Ecrire l'URL dans la BDD
+}
+
+const redirectTo = shortUrl => {
+	// Lire dans la BDD l'URL
+}
+
+const sendErr = error => {
+	res.send({ error })
+}
