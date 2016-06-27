ec0dee31274901313be8db5f555b9334acd571dd
programGitHub <programdev2000@gmail.com>
2016-06-27 16:18:04 +0200

    requests' logic

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ var app = express();
 
 app.get('/*', (req, res) => {
   res.send('URL SHORTENER MICROSERVICE');
-  parseUrl(req.url.substring(1))
+  parseUrl(req.url.substr(1))
 });
 
 app.listen('3000');
 
 const parseUrl = url => {
-	console.log(url)
-	
+
 	if (checkShort(url))
 		redirectTo(url)
 	else if (checkUrl(url))
@@ -19,14 +18,25 @@ const parseUrl = url => {
 		sendErr('invalid request')
 }
 
+const checkShort = url => {
+	return url.match(/^\d+$/)
+}
+
+const checkUrl = url => {
+	return url.toLowerCase().match(/^https?:\/\/(?:www.)?\w+\.[a-z]+(?::[0-9]+)?$/)
+}
+
 const shortUrl = url => {
+	console.log('shortUrl')
 	// Ecrire l'URL dans la BDD
 }
 
 const redirectTo = shortUrl => {
+	console.log('redirect')
 	// Lire dans la BDD l'URL
 }
 
 const sendErr = error => {
-	res.send({ error })
+	console.log(error)
+	// Renvoyer une erreur
 }
