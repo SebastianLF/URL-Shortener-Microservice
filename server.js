c7fdf43950665910f747efd4dead4908c296355a
slfweb <slf.pro@gmail.com>
2016-06-28 16:24:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,19 +12,17 @@ var app = express();
 
 app.get('/*', (req, res) => {
   res.send('URL SHORTENER MICROSERVICE');
-<<<<<<< HEAD
-=======
+
   parseUrl(req.url.substr(1))
->>>>>>> refs/remotes/origin/master
+
 });
 
 app.listen('3000');
 
 const parseUrl = url => {
-<<<<<<< HEAD
+
 	console.log(url)
-=======
->>>>>>> refs/remotes/origin/master
+
 
 	if (checkShort(url))
 		redirectTo(url)
@@ -43,14 +41,14 @@ const checkUrl = url => {
 }
 
 const shortUrl = url => {
-<<<<<<< HEAD
+
 	const newUrl = new Url({ original_url: url, short_url: process.env.BASE_URI + Date.now() });
   newUrl.save();
   return newUrl;
-=======
+
 	console.log('shortUrl')
 	// Ecrire l'URL dans la BDD
->>>>>>> refs/remotes/origin/master
+
 }
 
 const redirectTo = shortUrl => {
@@ -59,11 +57,10 @@ const redirectTo = shortUrl => {
 }
 
 const sendErr = error => {
-<<<<<<< HEAD
+
 	res.send({ error })
-}
-=======
+
 	console.log(error)
 	// Renvoyer une erreur
 }
->>>>>>> refs/remotes/origin/master
+
