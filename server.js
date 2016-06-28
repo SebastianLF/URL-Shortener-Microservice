80b5b775ebd4a6b7471955a74026bd71b64758c3
slfweb <slf.pro@gmail.com>
2016-06-28 21:15:23 +0200

    balise HEAD supprimées

--- server.js
+++ server.js
@@ -12,19 +12,13 @@ var app = express();
 
 app.get('/*', (req, res) => {
   res.send('URL SHORTENER MICROSERVICE');
-<<<<<<< HEAD
-=======
   parseUrl(req.url.substr(1))
->>>>>>> refs/remotes/origin/master
 });
 
 app.listen('3000');
 
 const parseUrl = url => {
-<<<<<<< HEAD
 	console.log(url)
-=======
->>>>>>> refs/remotes/origin/master
 
 	if (checkShort(url))
 		redirectTo(url)
@@ -43,14 +37,9 @@ const checkUrl = url => {
 }
 
 const shortUrl = url => {
-<<<<<<< HEAD
-	const newUrl = new Url({ original_url: url, short_url: process.env.BASE_URI + Date.now() });
+  const newUrl = new Url({ original_url: url, short_url: process.env.BASE_URI + Date.now() });
   newUrl.save();
   return newUrl;
-=======
-	console.log('shortUrl')
-	// Ecrire l'URL dans la BDD
->>>>>>> refs/remotes/origin/master
 }
 
 const redirectTo = shortUrl => {
@@ -59,11 +48,6 @@ const redirectTo = shortUrl => {
 }
 
 const sendErr = error => {
-<<<<<<< HEAD
 	res.send({ error })
 }
-=======
-	console.log(error)
-	// Renvoyer une erreur
-}
->>>>>>> refs/remotes/origin/master
+
